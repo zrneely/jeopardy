e54e4ef4e9056a636cd94842db5b00402e527e95
Zachary Neely <zrussellneely@gmail.com>
2020-04-14 21:23:03 -0700

    CSS for moderator controls

--- ts/controls.tsx
+++ ts/controls.tsx
@@ -10,15 +10,40 @@ interface ModeratorControlsProps {
 }
 export class ModeratorControls extends React.Component<ModeratorControlsProps> {
     render() {
+        let activityString;
+        switch (this.props.activity) {
+            case Activity.Moderate: {
+                activityString = 'Ask the controlling player to select a square.';
+                break;
+            }
+            case Activity.EvaluateAnswer: {
+                activityString = 'Wait for the active player to give an answer, then click' +
+                    ' correct or incorrect.';
+            }
+        }
+
         return <div className="moderator-controls">
-            <button disabled={this.props.activity !== Activity.EvaluateAnswer}>Correct Answer</button>
-            <button disabled={this.props.activity !== Activity.EvaluateAnswer}>Inorrect Answer</button>
-            <ul>
-                <li>Player in control: {this.props.controllingPlayer}</li>
-                <li>Player currently answering: {this.props.activePlayer}</li>
-                <li>Board seed: {this.props.seed}</li>
-            </ul>
-            <button onClick={this.props.newBoardClicked}>New Board</button>
+            <div className="moderator-controls-inner">
+                <div className="answer-eval-buttons">
+                    <button
+                        disabled={this.props.activity !== Activity.EvaluateAnswer}
+                        className="eval-button-correct">
+                        Correct
+                    </button>
+                    <button
+                        disabled={this.props.activity !== Activity.EvaluateAnswer}
+                        className="eval-button-incorrect">
+                        Inorrect
+                    </button>
+                </div>
+                <button onClick={this.props.newBoardClicked} className="new-board-button">New Board...</button>
+                <ul className="current-stats">
+                    <li>{activityString}</li>
+                    <li>Control: {this.props.controllingPlayer}</li>
+                    <li>Active: {this.props.activePlayer}</li>
+                    <li>Board Seed: <pre>{this.props.seed}</pre></li>
+                </ul>
+            </div>
         </div>;
     }
 }
